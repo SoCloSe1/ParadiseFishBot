492f1cd17a54f690789dfd8f9cf13c6c1b0ad1ce
SoCloSe1 <shahpesar22.sh@gmail.com>
2026-06-07 02:08:17 +0330

    first commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+print("شروع برنامه")
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام، به دستیار پارادیس فیش خوش اومدی 🐟")
+
+print("Bot Started")
+
+bot.infinity_polling()
